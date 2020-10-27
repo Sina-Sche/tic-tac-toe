4e4871815500da43cd741b5c5ba517a6ec1197b2
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-27 10:37:52 +0100

    Complete the game

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,18 +4,29 @@ import Square from "./Square";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(index) {
+    if (calculateWinner(squares) || squares[index]) {
+      return;
+    }
     const newSquares = squares.slice();
-    newSquares[index] = "🚀";
+    newSquares[index] = xIsNext ? "🚀" : "👨🏽‍🚀";
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
   }
 
   function renderSquare(index) {
     return <Square value={squares[index]} onClick={() => handleClick(index)} />;
   }
 
-  const status = "Next player: X";
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "🚀" : "👨🏽‍🚀");
+  }
 
   return (
     <div>
@@ -37,4 +48,28 @@ export default function Board() {
       </div>
     </div>
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
