6d6cb49e9892ce94e50e6cd772c80ef3190b665d
Sina-Sche <si.scheckenbach@gmail.com>
2020-10-27 11:57:52 +0100

    Refactor code

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,31 +1,33 @@
 import "./board.css";
 import React, { useState } from "react";
 import Square from "./Square";
+import { calculateWinner } from "../utils/winner";
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+  const [rocketIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(squares);
+  const nextPlayer = rocketIsNext ? "🚀" : "👨🏽‍🚀";
 
   function handleClick(index) {
     if (calculateWinner(squares) || squares[index]) {
       return;
     }
     const newSquares = squares.slice();
-    newSquares[index] = xIsNext ? "🚀" : "👨🏽‍🚀";
+    newSquares[index] = nextPlayer;
     setSquares(newSquares);
-    setXIsNext(!xIsNext);
+    setXIsNext(!rocketIsNext);
   }
 
   function renderSquare(index) {
     return <Square value={squares[index]} onClick={() => handleClick(index)} />;
   }
 
-  const winner = calculateWinner(squares);
   let status;
   if (winner) {
     status = "Winner: " + winner;
   } else {
-    status = "Next player: " + (xIsNext ? "🚀" : "👨🏽‍🚀");
+    status = "Next player: " + nextPlayer;
   }
 
   return (
@@ -48,28 +50,4 @@ export default function Board() {
       </div>
     </div>
   );
-
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
 }
